897221a24824f61180261ebcf15a756d1d91be6e
vagrant <vagrant@localhost.localdomain>
2020-07-05 04:32:03 +0000

    devise-install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers:{
+  	sessions: 'admins/sessions'
+  }
+  devise_for :users
+  resource :users
+  get 'homes/top' => 'homes#top'
+  root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
