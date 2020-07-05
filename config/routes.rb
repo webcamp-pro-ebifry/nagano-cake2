40de6bd5ead4345f23908f297b925a11e351c5bd
vagrant <vagrant@localhost.localdomain>
2020-07-05 10:53:08 +0000

    管理者側ユーザー一覧詳細編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  namespace :admins do
+   resources :users, only: [:index,:show,:edit,:update]
+  end
+  
   devise_for :admins, controllers:{
   	sessions: 'admins/sessions'
   }
   devise_for :users
   resource :users
   get 'homes/top' => 'homes#top'
+  get 'homes/about' => 'homes#about'
+
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
