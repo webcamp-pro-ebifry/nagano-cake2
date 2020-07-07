9bb43aba897a52ecceb8437bf899e69af77e21b4
vagrant <vagrant@localhost.localdomain>
2020-07-07 02:19:43 +0000

    edit

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -3,8 +3,25 @@ class Admins::ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save(name: params[:product][:name], status: params[:product][:genre_id].to_i, status: params[:product][:product_status].to_i) 
+      flash[:notice] = "successfully"
+      redirect_to admins_products_path(@product)
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :introduce, :image_id, :price, :genre_id, :product_status, :delete_flag)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-        enum product_status: {
-            sale: 0,
-            soldout: 1
-        },_prefix: true
+    attachment :image
+
+    enum product_status: {
+        "販売中": 0,
+        "売り切れ": 1
+    },_prefix: true
 end
