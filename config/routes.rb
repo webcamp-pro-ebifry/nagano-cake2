d1c42fa699e1172ebe83c5705a2e1dde906e1294
vagrant <vagrant@localhost.localdomain>
2020-07-09 02:25:36 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resource :users
   devise_for :admins, controllers:{
   	sessions: 'admins/sessions'
   }
   devise_for :users
-  resource :users
+  
   get 'homes/top' => 'homes#top'
   root 'homes#top'
+  resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
