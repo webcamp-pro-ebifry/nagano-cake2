d5b33597761b73fdb7d74db5fee7383d45ccee90
vagrant <vagrant@localhost.localdomain>
2020-07-14 07:35:53 +0000

    本日の注文件数表示

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,4 +1,8 @@
 class Admins::HomesController < ApplicationController
 	def top
+    # 今日の注文件数をカウント
+    start_date = Time.current.beginning_of_day
+    end_date = Time.current.end_of_day
+    @counts = Order.where(created_at: start_date..end_date).count
 	end
 end
